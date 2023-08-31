d37598d71d7133dc6dd39aaee50e046a0ddd1342
Andrew vonderLuft <wonder@hey.com>
2023-08-31 15:05:33 -0700

    Updating db migrations

--- db/migrate/00_create_active_storage_tables.rb
+++ db/migrate/00_create_active_storage_tables.rb
@@ -7,10 +7,14 @@ class CreateActiveStorageTables < ActiveRecord::Migration[6.0]
       t.string   :service_name, null: false
       t.text     :metadata
       t.bigint   :byte_size,  null: false
-      t.string   :checksum,   null: false
+      t.string   :checksum,   null: true
       t.datetime :created_at, null: false
 
       t.index [ :key ], unique: true
+    end
+
+    if configured_service = ActiveStorage::Blob.service.name
+      ActiveStorage::Blob.unscoped.update_all(service_name: configured_service)
     end
 
     create_table :active_storage_attachments do |t|
@@ -23,12 +27,24 @@ class CreateActiveStorageTables < ActiveRecord::Migration[6.0]
       t.index [ :record_type, :record_id, :name, :blob_id ], name: "index_active_storage_attachments_uniqueness", unique: true
     end
 
-    create_table :active_storage_variant_records do |t|
-      t.belongs_to :blob, null: false, index: false
+    create_table :active_storage_variant_records, id: primary_key_type, if_not_exists: true do |t|
+      t.belongs_to :blob, null: false, index: false, type: blobs_primary_key_type
       t.string :variation_digest, null: false
 
       t.index %i[ blob_id variation_digest ], name: "index_active_storage_variant_records_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
   end
+
+  private
+    def primary_key_type
+      config = Rails.configuration.generators
+      config.options[config.orm][:primary_key_type] || :primary_key
+    end
+
+    def blobs_primary_key_type
+      pkey_name = connection.primary_key(:active_storage_blobs)
+      pkey_column = connection.columns(:active_storage_blobs).find { |c| c.name == pkey_name }
+      pkey_column.bigint? ? :bigint : pkey_column.type
+    end
 end
